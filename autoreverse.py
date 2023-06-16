49973894cecedb36ce6d52ec25c19ad1569840e6
CronoX1 <91015518+CronoX1@users.noreply.github.com>
2023-06-17 00:58:11 +0200

    Update autoreverse.py

--- autoreverse.py
+++ autoreverse.py
@@ -35,7 +35,6 @@ print(yellow('Autoreverse made by CronoX\n\nhttps://github.com/CronoX1\n--------
 
 if os.popen('which autoreverse').read().strip('\n') == '':
     print(blue('\nCreating a symbolik link so you can use the tool in all directories (autoreverse).'))
-    os.system('dos2unix autoreverse.py 2>/dev/null')
     os.system('chmod +x autoreverse.py')
     os.system('ln -s ' + ActualPath + '/autoreverse.py $(echo $(echo $PATH | cut -d ":" -f1)/autoreverse)')
 
